76fe275bbb1f4be60d38361bcd97dce4fecee515
cbarrios <lalobarrios16@yahoo.com>
2022-08-01 18:09:37 -0400

    master: (4) Display list

---
+++ app/src/main/java/com/lalosapps/superheroesapp/HeroesScreen.kt
@@ -0,0 +1,70 @@
+package com.lalosapps.superheroesapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.lalosapps.superheroesapp.model.Hero
+
+@Composable
+fun HeroList(heroes: List<Hero>, modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier.background(MaterialTheme.colors.background)
+    ) {
+        items(heroes) {
+            HeroItem(hero = it)
+        }
+    }
+}
+
+@Composable
+fun HeroItem(hero: Hero, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+        elevation = 2.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .sizeIn(minHeight = 72.dp)
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(hero.nameRes),
+                    style = MaterialTheme.typography.h3
+                )
+                Text(
+                    text = stringResource(hero.descriptionRes),
+                    style = MaterialTheme.typography.body1
+                )
+            }
+            Spacer(Modifier.width(16.dp))
+            Box(
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                Image(
+                    painter = painterResource(hero.imageRes),
+                    contentDescription = null,
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/lalosapps/superheroesapp/MainActivity.kt
+++ app/src/main/java/com/lalosapps/superheroesapp/MainActivity.kt
@@ -3,13 +3,9 @@ package com.lalosapps.superheroesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.lalosapps.superheroesapp.data.HeroesRepository.heroes
 import com.lalosapps.superheroesapp.ui.theme.SuperheroesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +13,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SuperheroesAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                HeroList(heroes)
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
+fun AppPreview() {
     SuperheroesAppTheme {
-        Greeting("Android")
+        HeroList(heroes)
     }
 }
